58c1e2efc3f736189322854ce03c658dbbb6098b
Pablo Dominguez <pasaxo@gmail.com>
2024-01-09 17:00:45 -0300

    testando enviar imagem com promt

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,30 +1,52 @@
 import express from "express";
 import OpenAI from "openai";
 import * as dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import base64 from 'base-64';
+
 dotenv.config();
 
 const app = express();
 
-app.post('/', async (req, res) => {
+const openai = new OpenAI({
+    key: process.env.OPENAI_API_KEY,
+    engine: 'gpt-4-vision-preview',
+});
 
-    const { prompt } = req.query;	
+function encodeImage(imagePath) {
+    const imageContent = fs.readFileSync(imagePath, { encoding: 'base64' });
+    return base64.encode(imageContent);
+}
 
-    // const openai = new OpenAI({
-    //     key: process.env.OPENAI_API_KEY,
-    //     organization: 'org-3yThFQYfStDZnjLBPzK7OBhZ',
-    // });
+const imagePath = path.join('ticket.jpeg');
 
-    const openai = new OpenAI();
+const base64Image = encodeImage(imagePath);
 
-    const image = await openai.images.generate({
-        model: "dall-e-3",
-        prompt: prompt,
+async function main() {
+    const response = await openai.chat.completions.create({
+        model: "gpt-4-vision-preview",
+        messages: [
+            {
+                role: "user",
+                content: [
+                    { type: "text", text: "Analyze the following receipt and provide details in a spreadsheet format:" },
+                    {
+                        type: "image_url",
+                        image_url: {
+                            url: `data:image/jpeg;base64,${base64Image}`,
+                        },
+                    },
+                ],
+            },
+        ],
+        max_tokens: 300,
     });
 
-    console.log(image.data);
+    console.log(response.choices[0]);
+}
 
-    res.send(image.data);
-});
+main();
 
 app.listen(4001, () => {
     console.log('Server is running on port 4001');
